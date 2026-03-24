2faf1dddc1dd5ea1e39e0a345ce6286f649b713c
Haru Ichi <jarunee.recc2@gmail.com>
2026-03-24 23:42:24 +0700

    update seed

--- backend/seed/createAdmin.js
+++ backend/seed/createAdmin.js
@@ -2,25 +2,31 @@ const bcrypt = require("bcrypt");
 const db = require("../helper/db");
 
 async function createAdmin() {
-  // 🔍 เช็คก่อนว่ามี admin แล้วหรือยัง
-  const [rows] = await db.query(
-    "SELECT * FROM users WHERE username = ?",
-    ["admin"]
-  );
+  try {
+    // 🔍 เช็คก่อนว่ามี admin แล้วหรือยัง
+    const [rows] = await db.query("SELECT * FROM users WHERE username = ?", [
+      "admin",
+    ]);
 
-  if (rows.length > 0) {
-    console.log("⚠️ Admin already exists");
-    return;
+    if (rows.length > 0) {
+      console.log("⚠️ Admin already exists");
+      return;
+    }
+
+    const hash = await bcrypt.hash("1234", 10);
+
+    await db.query(
+      "INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
+      ["admin", hash, "admin"]
+    );
+
+    console.log("✅ admin created (user: admin / pass: 1234)");
+  } catch (error) {
+    console.error("❌ Error:", error);
+  } finally {
+    // ปิด process หลังจากทำทุกอย่างเสร็จ
+    process.exit(0);
   }
-
-  const hash = await bcrypt.hash("1234", 10);
-
-  await db.query(
-    "INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
-    ["admin", hash, "admin"]
-  );
-
-  console.log("✅ admin created (user: admin / pass: 1234)");
 }
 
 createAdmin();
