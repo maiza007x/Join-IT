6b56e6f11547781a4f85360041819bada483baf4
Haru Ichi <jarunee.recc2@gmail.com>
2026-04-10 14:35:12 +0700

    add default socket ip

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // URL ของ Backend API
-const SOCKET_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const SOCKET_URL = import.meta.env.VITE_API_BASE_URL || "http://172.16.39.6:5000";
 
 const socket = io(SOCKET_URL, {
     autoConnect: true,
