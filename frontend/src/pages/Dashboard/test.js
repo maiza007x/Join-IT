17202a44cd9cc98f384cd42f7e21c0d79da05bfd
Duan <anuruksd@gmail.com>
2026-05-26 10:35:59 +0700

    111

---
+++ frontend/src/pages/Dashboard/test.js
@@ -0,0 +1,25 @@
+เครื่องหมายถูก
+drawCheckmark(เลขหน้า, "แกน x", "แกน y")
+ex.drawCheckmark(page2, 116, 241)
+ตัวหนังสือ
+drawText(เลขหน้า, "คำที่ต้องการแสดง", "แกน x", "แกน y", 10);
+drawText(page3, "นายกอ ขอคอ", 150, 496, 10);
+drawText(page3, "นายกอ ขอคอ", 150, 496, 10);
+drawText(page3, 'Achiraya Jampawan', 217, 506, 10);
+drawText(page3, '119', 422, 506, 10);
+drawText(page3, '10', 486, 506, 10);
+drawText(page3, '20', 122, 483, 10);
+drawText(page3, 'December', 169, 483, 10);
+drawText(page3, '2004', 267, 483, 10);
+drawText(page3, '1234567891111', 375, 483, 10);
+drawText(page3, 'Umphangwittayakom', 142, 461, 10);
+drawText(page3, 'Duan Anurak', 217, 411, 10);
+drawText(page3, '0321654987', 125, 389, 10);
+drawText(page3, 'duan@gmail.com', 304, 389, 10);
+drawText(page3, 'Grandma', 267, 367, 10);
+drawText(page3, 'Anurak Duan', 231, 345, 10);
+drawText(page3, 'Duan Anurak', 343, 279, 10);
+drawText(page3, 'Duan Anurak', 307, 257, 10);
+drawText(page3, '20', 318, 236, 10);
+drawText(page3, 'September', 385, 236, 10);
+drawText(page3, '2026', 477, 236, 10);
