f6f2909a38ecc2f9e7ef68a9b30894d7999349fa
Duan <anuruksd@gmail.com>
2026-05-26 15:40:17 +0700

    Update

--- frontend/src/pages/Dashboard/test.js
+++ frontend/src/pages/Dashboard/test.js
@@ -23,3 +23,71 @@ drawText(page3, 'Duan Anurak', 307, 257, 10);
 drawText(page3, '20', 318, 236, 10);
 drawText(page3, 'September', 385, 236, 10);
 drawText(page3, '2026', 477, 236, 10);
+
+drawText(page1, '20', 506, 783, 10); //dd/mm/yr
+drawText(page1, '12', 531, 783, 10); //dd/mm/yr
+drawText(page1, '2026', 553, 784, 10); //dd/mm/yr
+drawCheckmark(page1, 22, 765); //Frist Time
+drawCheckmark(page1, 149, 766); //Repeat
+drawCheckmark(page1, 358, 766); //Regular
+drawText(page1, 'Anurak Duan', 110, 658, 10); //Surname
+drawText(page1, 'Duan Anurak', 128, 641, 10); //Previously
+drawText(page1, '8', 170, 612, 10); //CITIZEN ID
+drawText(page1, '9', 188, 612, 10); //CITIZEN ID
+drawText(page1, '7', 206, 612, 10); //CITIZEN ID
+drawText(page1, '6', 224, 612, 10); //CITIZEN ID
+drawText(page1, '5', 242, 612, 10); //CITIZEN ID
+drawText(page1, '4', 260, 612, 10); //CITIZEN ID
+drawText(page1, '3', 279, 612, 10); //CITIZEN ID
+drawText(page1, '2', 297, 612, 10); //CITIZEN ID
+drawText(page1, '1', 315, 612, 10); //CITIZEN ID
+drawText(page1, '0', 333, 612, 10); //CITIZEN ID
+drawText(page1, '9', 351, 612, 10); //CITIZEN ID
+drawText(page1, '8', 369, 612, 10); //CITIZEN ID
+drawText(page1, '7', 387, 612, 10); //CITIZEN ID
+drawText(page1, '28', 488, 608, 10); //Date of birth
+drawText(page1, '12', 515, 608, 10); //Date of birth
+drawText(page1, '26', 543, 608, 10); //Date of birth
+drawText(page1, '192168', 96, 592, 10); //Blood Donor ID
+drawText(page1, '100', 263, 592, 10); //Age
+drawText(page1, '130', 372, 592, 10); //Weight Kg
+drawText(page1, '200', 481, 592, 10); //Height Cm
+drawCheckmark(page1, 116, 577); //female
+drawCheckmark(page1, 208, 577); //male
+drawCheckmark(page1, 116, 558); //Woman
+drawCheckmark(page1, 208, 558); //man
+drawCheckmark(page1, 284, 558); //transwoman
+drawCheckmark(page1, 382, 558); //tranman
+drawCheckmark(page1, 108, 532); //Not changed
+drawCheckmark(page1, 205, 532); //Changed as follows
+drawText(page1, '212/224 Nongloung Umphang Tak', 309, 528, 10); //Postal
+drawText(page1, '63110', 465, 511, 10); //Code
+drawText(page1, '0321654987', 105, 495, 10); //Mobile
+drawText(page1, '0987654321', 308, 495, 10); //Contact
+drawText(page1, 'anurakanurak@gmail.com', 434, 495, 10); //E-mail
+drawCheckmark(page1, 99, 472); //Student
+drawCheckmark(page1, 191, 472); //Gov official
+drawCheckmark(page1, 438, 472); //Employee
+drawCheckmark(page1, 99, 454); //Monk,Priest
+drawCheckmark(page1, 191, 454); //Agriculturist
+drawCheckmark(page1, 300, 454); //Business
+drawCheckmark(page1, 438, 454); //Others
+drawText(page1, 'Anurak', 484, 450, 10); //Others Text
+drawCheckmark(page1, 91, 406); //Whole Blood
+drawCheckmark(page1, 212, 406); //Apheresis
+drawCheckmark(page1, 213, 386); //Red cells
+drawCheckmark(page1, 280, 386); //Platelets
+drawCheckmark(page1, 355, 386); //Plasmap
+drawCheckmark(page1, 441, 386); //Others02
+drawText(page1, 'Anurakduan', 489, 382, 10); //Others Text02
+drawCheckmark(page1, 212, 368); //No complication
+drawCheckmark(page1, 36, 349); //complication
+drawCheckmark(page1, 129, 348); //Fainting
+drawCheckmark(page1, 213, 348); //Bruise
+drawCheckmark(page1, 281, 348); //Phlebotomy
+drawCheckmark(page1, 393, 348); //Others03
+drawCheckmark(page1, 36, 330); //Not allowed
+drawCheckmark(page1, 213, 329); //Low Hemoglobin
+drawCheckmark(page1, 394, 329); //Others04
+drawText(page1, 'Anurakanurak', 442, 345, 10); //Others
+drawText(page1, 'Duanduanduan', 442, 325, 10); //Others
